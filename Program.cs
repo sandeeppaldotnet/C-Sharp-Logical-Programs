84085244fb3eb4f830013fb7ee57076f447b8f95
sandeep084991904 <sandeep084991904@gmail.com>
2023-08-01 11:06:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Swapping_Program_in_C__with_Examples swap=new Swapping_Program_in_C__with_Examples ();
+            swap.swap();
+        }
+    }
+}
---
+++ Swapping Program in C# with Examples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Logical_Programs
+{
+    public class Swapping_Program_in_C__with_Examples
+    {
+        int num1=100,num2=200;
+        int temp = 0;
+       
+        public void swap()
+        {
+            Console.WriteLine($"Before Swapping : {num1}, {num2}");
+            temp = num1;    
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine($"After Swapping : {num1}, {num2}");
+            Console.ReadLine();
+        }
+           
+    }
+}
